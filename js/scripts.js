a547e350ed28b9d6ccf25132a49fe9b6f8d95f85
Yi Chiang and russ vetsper <yi+russ@>
2016-06-21 10:54:57 -0700

    refine looping

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var languages = ['HTML', 'CSS', 'JavaScript','JavaScript','JavaScript','JavaScript'];
+for(var i=0; i<languages.length; i++){
+  console.log(languages[i]);
+alert("I love"  + languages[i] + "!")
+}
